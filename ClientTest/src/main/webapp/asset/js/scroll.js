87faf4788500d19511a320ef9806cf9e8fbfe083
eeunha <eunha.dev@gmail.com>
2023-10-16 12:48:46 +0900

    231016 오전수업

---
+++ ClientTest/src/main/webapp/asset/js/scroll.js
@@ -0,0 +1,28 @@
+//scroll.js
+
+//1. <div id="bar"></div> 동적 추가
+//2. css 적용
+//3. 이벤트 추가
+
+$('<div id="scroll-bar-indicator"></div>')
+  .css({
+    width: 0,
+    height: "5px",
+    backgroundColor: "cornflowerblue",
+    position: "fixed",
+    left: 0,
+    top: 0,
+    /* 문제될 css 수정하기 */
+    border: 0,
+    padding: 0,
+    margin: 0,
+  })
+  .prependTo($("body"));
+
+$(document).scroll(function () {
+  let x =
+    ($(document).scrollTop() * 100) /
+    ($(document).outerHeight() - $(window).outerHeight());
+
+  $("#scroll-bar-indicator").css("width", x + "%");
+});
